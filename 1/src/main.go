1d71b3a6ebd684aae10284e86a3c3021b468d26c
Peter Hecht Glad <tremble@Peters-MacBook-Air.local>
2019-08-04 02:14:33 +0200

    sure

--- 1/src/main.go
+++ 1/src/main.go
@@ -1,7 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	println("Cool World!")
+	if len(os.Args) != 2 {
+		os.Exit(1)
+	}
+	var theArg string
+	theArg = os.Args[1]
+	fmt.Println("Cool World!", theArg)
+	printMoreShit()
+}
+
+func printMoreShit() {
+	v1, v2, v3 := "1", 2, "3"
+	fmt.Printf("%s - %d - %s", v1, v2, v3)
 }
 
 // https://gobyexample.com/
